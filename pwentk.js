cc73f5c11fbe5027a15867274b2ae63507a00b00
EntryDark <entrydark@gmail.com>
2019-02-12 17:26:56 +0900

    pwentk.Block에서 함수 인자 있는 경우 모양 변경토록

--- pwentk.js
+++ pwentk.js
@@ -166,41 +166,62 @@ var pwentk = {
 
 	},
 	BlockSet: class{
-		constructor(name, text, func){
-			if(name&&text&&func){
-				this.name = name;
-				this.text = text;
-				this.func = func;
-				this.param = 
-				pwentk.nArray(this.text.match(/(?<=\(\()(.*?)(?=\)\))/g))
-				.map(name => ({name: name, type: "string", data: "0"}))
-				.concat(
-					pwentk.nArray(this.text.match(/(?<=\<\<)(.*?)(?=\>\>)/g))
-					.map(name => ({name: name, type: "boolean", data: true}))
-					)
-				.concat(
-					pwentk.nArray(this.text.match(/(?<=\{\{)(.*?)(?=\}\})/g))
-					.map(name => ({name: name, type: "function", data: function(){}}))
-					);
-			}else{
-				pwentk.genErr(20);
-			}
+		constructor(options){
+			this.name = options.name;
+			this.text = options.text;
+			this.func = options.func;
+			this.param = 
+			pwentk.nArray(this.text.match(/(?<=\(\()(.*?)(?=\)\))/g))
+			.map(name => ({name: name, type: "string", data: "0"}))
+			.concat(
+				pwentk.nArray(this.text.match(/(?<=\<\<)(.*?)(?=\>\>)/g))
+				.map(name => ({name: name, type: "boolean", data: true}))
+				)
+			.concat(
+				pwentk.nArray(this.text.match(/(?<=\{\{)(.*?)(?=\}\})/g))
+				.map(name => ({name: name, type: "function", data: function(){}}))
+				);
 		}
 	},
 	Block: class{
 		constructor(parent){
-
+			this.name = parent.name;
+			this.text = parent.text;
+			this.param = parent.param;
+			this.innerBlock = this.param.filter(val=>val.type=="function")
 		}
 		getPath(text){
-			return `M0 0 l 5 5 v -4 
+			var output = "";
+			output += `M0 0 l 5 5 v -4 
+			a 1 1 90 0 1 1 -1  
+			h 20
+			a 5 5 180 0 1 0 10 `;
+			output += `H 11 `;
+
+			if(this.innerBlock.length > 0){
+				output += `a 1 1 90 0 0 -1 1
+				v 4
+				l -5 -5`;
+				for(var i=0;i<this.innerBlock.length-1;i++){
+					output += `v 10 
+					l 5 5 v -4 
 					a 1 1 90 0 1 1 -1  
-					h 20
+					h 20 
 					a 5 5 180 0 1 0 10 
 					H 11 
 					a 1 1 90 0 0 -1 1 
-					v 4 l -5 -5
-					H 5 
-					v 5 l -5 -5 Z`;
+					v 4 
+					l -5 -5 `;
+				}
+				output += `v 10 
+				l 5 5 v -4 
+				a 1 1 90 0 1 1 -1  
+				h 20 
+				a 5 5 180 0 1 0 10 `;
+			}
+			output += `H 6 a 1 1 90 0 0 -1 1 v 5 l -5 -5
+			Z`;
+			return output;
 		}
 	},
 	genErr: function(errCode, errSprite){
