364aa0bf52d473c68595af321b1f64596dd754c8
EntryDark <entrydark@gmail.com>
2019-03-19 23:55:18 +0900

    UI 불필요 동기화 코드 삭제

--- docs/make/ui.js
+++ docs/make/ui.js
@@ -106,9 +106,5 @@ var app = new Vue({
 	}
 });
 
-pwentk.on("sceneChanged", function(project){
-	app.scenes = project.scenes;
-	app.activeScene = project.nowSceneNo;
-});
 window.addEventListener("resize", canvasResize);
 window.addEventListener("DOMContentLoaded", canvasResize);
